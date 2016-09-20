b901de3b785d46edb120c71b267ad0ce9637001d
刘旭笙 <liuxusheng@mir-crazydemacbook-pro.local>
2016-09-20 14:57:44 +0800

    创建了狗类

---
+++ budejue/budejue/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  budejue
+//
+//  Created by 刘旭笙 on 16/9/20.
+//  Copyright © 2016年 MrCrazy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ budejue/budejue/Dog.m
@@ -0,0 +1,22 @@
+//
+//  Dog.m
+//  budejue
+//
+//  Created by 刘旭笙 on 16/9/20.
+//  Copyright © 2016年 MrCrazy. All rights reserved.
+//
+
+#import "Dog.h"
+
+@interface Dog ()
+
+@property(nonatomic, strong)NSString *name;
+
+@end
+
+
+@implementation Dog 
+
+
+
+@end
